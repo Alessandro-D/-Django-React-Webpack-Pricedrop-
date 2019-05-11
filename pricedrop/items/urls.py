c4a4fdb29823224c23d123b38dbcd84f057c8de0
xandro <a_dangeli91@hotmail.it>
2019-05-11 16:48:34 +0200

    pylint

--- pricedrop/items/urls.py
+++ pricedrop/items/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('api/item/', views.ItemListCreate.as_view() ),
+        path('api/item', views.ItemListCreate.as_view() ),
 ]
